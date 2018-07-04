0f89f67acefbc0784ea5cfa241c7496be890e34d
Unmesh Desale <udesale@redhat.com>
2018-07-04 18:41:15 +0530

    adding multi config support

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
+from os import os
 
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+print(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
